bd8720cf74f2b24705e0cbc6732b2a1933796e0f
Maxim Hasanau <maxlinus1408@gmail.com>
2023-04-21 02:16:20 +0300

    feat: add fonts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log('hello')
+import './assets/sass/style.scss';
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'assets/scripts/bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext][query]',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -14,5 +18,30 @@ module.exports = {
       template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
-  ]
+    new MiniCssExtractPlugin({
+      filename: 'assets/styles/style.css'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[hash][ext][query]',
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[hash][ext][query]',
+        },
+      },
+    ],
+  },
 };
