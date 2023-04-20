e67a6a2b03b69e3cc172fadc3a09f3e568cc3f69
Maxim Hasanau <maxlinus1408@gmail.com>
2023-04-21 00:48:02 +0300

    feat: add Webpack and configure the project

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'assets/scripts/bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ]
+};
